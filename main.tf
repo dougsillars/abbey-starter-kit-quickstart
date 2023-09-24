9121193eef254f95c0de35a0a5fa7735e7e4c8cf
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-09-24 18:44:16 -0400

    accessto abbey repo

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "abbey_grant_kit" "abbey_demo_site" {
   output = {
     # Replace with your own path pointing to where you want your access changes to manifest.
     # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://dougsillars/dougsillars/access.tf" # CHANGEME
+    location = "github://dougsillars/abbey-starter-kit-quickstart/access.tf" # CHANGEME
     append = <<-EOT
       resource "abbey_demo" "grant_read_write_access" {
         permission = "read_write"
