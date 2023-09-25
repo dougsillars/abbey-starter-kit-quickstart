cccb2b55284308b43302a5cba63eaa958de49e6b
Abbey <no-reply@abbey.io>
2023-09-25 22:03:41 +0000

    doug.sillars@gmail.com requests access to: Abbey_Demo_Site_full_access5

--- access.tf
+++ access.tf
@@ -2,3 +2,7 @@ resource "abbey_demo" "grant_read_write_access" {
   permission = "read_write"
   email = "doug.sillars@gmail.com"
 }
+resource "abbey_demo" "grant_read_write_access_2023-09-25T21:55:50Z" {
+  permission = "read_write"
+  email = "doug.sillars@gmail.com"
+}
