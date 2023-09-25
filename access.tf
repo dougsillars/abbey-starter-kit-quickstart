ef774e62d76abfaf9981ff87f2c43c38e6cd7297
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-09-25 18:22:12 -0400

    request6

--- access.tf
+++
@@ -1,8 +0,0 @@
-resource "abbey_demo" "grant_read_write_access" {
-  permission = "read_write"
-  email = "doug.sillars@gmail.com"
-}
-resource "abbey_demo" "grant_read_write_access_usu3" {
-  permission = "read_write"
-  email = "doug.sillars@gmail.com"
-}
