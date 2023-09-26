92b13dab0945becc7ab1820395d7245dddb14639
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-09-26 17:19:38 -0400

    request23

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "random_string" "random_suffix" {
 
 locals {
   email = "{{.data.system.abbey.identities.abbey.email}}"
-  sanitized_email = regex("[a-zA-Z]+", local.email)
+  sanitized_email =  local.email
 }
 
 
